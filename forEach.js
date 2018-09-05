33b55770c1e0315a7f1722fd2f029426563b1e83
thanhcong051593 <42972932+thanhcong051593@users.noreply.github.com>
2018-09-05 08:30:30 +0700

    Update forEach.js

--- forEach.js
+++ forEach.js
@@ -1,1 +1,12 @@
+let numbers = [1,2,4,5,7,8,5,6,20,10];
 
+l copy = numbers.forEach(function(i){
+	return i + 0.7;
+});
+
+console.log(copy);// undefined
+console.log(numbers); //[ 1, 2, 4, 5, 7, 8, 5, 6, 20, 10 ]
+
+numbers.forEach(function(i){
+	console.log(i);
+});
